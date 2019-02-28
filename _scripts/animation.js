fc802bbe95ad2644079c42457a57fef33a72183d
George Pearson <george.pearson@iglu.com>
2019-02-28 22:43:11 +0000

    Center SVG

--- _scripts/animation.js
+++ _scripts/animation.js
@@ -1,6 +1,6 @@
 "use strict";
 $(document).ready(function(){
-    var svg = document.querySelector('#maboi-drawself');
+    var svg = document.querySelector('.maboi-drawself');
     var lines = Array.from(svg.querySelectorAll('.line'));
     var shades = Array.from(svg.querySelectorAll('.shade'));
     var paths = [...lines, ...shades];
@@ -13,9 +13,9 @@ $(document).ready(function(){
         return Math.pow(length, 0.5) * 0.03;
     });
 
-    // triggering a reflow so styles are calculated in their
-    // start position, so they animate from here
+    // triggering a reflow so we animate from first path
     paths[0].getBoundingClientRect();
+    
     var begin = 0;
     paths.forEach(function(path, i) {
         path.style.transition = `stroke-dashoffset ${durations[i]}s ${begin}s ease-in-out, fill-opacity ${durations[i]}s ${begin}s ease-in-out`;
