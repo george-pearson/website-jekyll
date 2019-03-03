e755abb58b13f5b060ca4a5d1811c19783a5f9e0
George Pearson <george.pearson@iglu.com>
2019-03-03 23:07:02 +0000

    Add Intro and Re-Write animation.js

--- _scripts/animation.js
+++ _scripts/animation.js
@@ -1,26 +1,53 @@
 "use strict";
 $(document).ready(function(){
-    var svg = document.querySelector('.maboi-drawself');
-    var lines = Array.from(svg.querySelectorAll('.line'));
-    var shades = Array.from(svg.querySelectorAll('.shade'));
-    var paths = [...lines, ...shades];
-    
-    var durations = paths.map(function(path) {
-        var length = path.getTotalLength();
-        path.style.strokeDasharray = length + ' ' + length;
-        path.style.strokeDashoffset = length;
-        path.style.fillOpacity = '0';
-        return Math.pow(length, 0.5) * 0.03;
+    /*----------Maboi----------*/
+    var maboi = document.querySelector(".maboi-drawself");
+    var maboiLines = Array.from(maboi.querySelectorAll(".line"));
+    var maboiShades = Array.from(maboi.querySelectorAll(".shade"));
+    var maboiPaths = [...maboiLines, ...maboiShades];
+    var maboiAnimations = maboiPaths.map(function(path){
+        var pathLength = path.getTotalLength();
+        var duration = Math.pow(pathLength, 0.5) * 0.03;
+        return {path, pathLength, duration};
     });
 
-    // triggering a reflow so we animate from first path
-    paths[0].getBoundingClientRect();
-    
-    var begin = 0;
-    paths.forEach(function(path, i) {
-        path.style.transition = `stroke-dashoffset ${durations[i]}s ${begin}s ease-in-out, fill-opacity ${durations[i]}s ${begin}s ease-in-out`;
-        path.style.strokeDashoffset = '0';
-        path.style.fillOpacity = '1.0';
-        begin += durations[i] + 0.1;
-    });
+    function runMaboiAnimations(){
+        // Initial conditions
+        maboiAnimations.forEach(function(animation){
+            animation.path.style.transition = "none";
+            animation.path.style.strokeDasharray = `${animation.pathLength} ${animation.pathLength}`;
+            animation.path.style.strokeDashoffset = animation.pathLength;
+            animation.path.style.fillOpacity = "0";
+            animation.path.getBoundingClientRect();
+        });
+
+        // Triggering a reflow so we animate from first path
+        maboiAnimations[0].path.getBoundingClientRect();
+
+        // Run line animations
+        var begin = 0;
+        maboiAnimations.forEach(function(animation) {
+            animation.path.style.transition = `stroke-dashoffset ${animation.duration}s ${begin}s ease-in-out, fill-opacity ${animation.duration}s ${begin}s ease-in-out`;
+            animation.path.style.strokeDashoffset = "0";
+            animation.path.style.fillOpacity = "1.0";
+            begin += animation.duration + 0.1;
+        });
+    }
+    maboi.addEventListener("click", runMaboiAnimations);
+    runMaboiAnimations();
+    /*----------Coffee----------*/
+    function toggleAnimationPlayState(element){
+        if (element.style.animationPlayState === 'running') {
+            element.style.animationPlayState = 'paused';
+        } else {
+            element.style.animationPlayState = 'running';
+        }
+    }
+    var coffeeDashed = document.querySelector(".coffee-dashed");
+    var coffeeDashedPath = document.querySelector(".coffee-dashed path");
+    coffeeDashed.addEventListener("click", () => toggleAnimationPlayState(coffeeDashedPath));
+
+    var coffeeDrawSelf = document.querySelector(".coffee-drawself");
+    var coffeeDrawSelfPath = document.querySelector(".coffee-drawself path");
+    coffeeDrawSelf.addEventListener("click", () => toggleAnimationPlayState(coffeeDrawSelfPath));
 });
