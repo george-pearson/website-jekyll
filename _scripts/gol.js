a854410d1e981fe5567ecb988ffb11fbda41feea
George Pearson <george.pearson@iglu.com>
2019-04-01 21:21:59 +0100

    Further Re-factor

--- _scripts/gol.js
+++ _scripts/gol.js
@@ -4,63 +4,62 @@ $(document).ready(function(){
   var DEAD = 0;
   var N = 250;
   var arr = create2Darray(N, N, DEAD);
+  var canvas = document.querySelector("#myCanvas");
+  canvas.width = arr.length;
+  canvas.height = arr[0].length;
+  var ctx = canvas.getContext("2d");
   initialise_cross(arr);
-  
   print_to_canvas(arr);
   
-  //Buttons:
   var requestId;
-  $("#start-stop").click(function start() {
-      if (!requestId) {
-        requestId = window.requestAnimationFrame(gameLoop);
-      }
-      else {
-        window.cancelAnimationFrame(requestId);
-        requestId = undefined;
-      }
+  document.querySelector("#start-stop").addEventListener('click', () => {
+    if (!requestId) {
+      requestId = window.requestAnimationFrame(gameLoop);
+    }
+    else {
+      window.cancelAnimationFrame(requestId);
+      requestId = undefined;
+    }
   });
-  $("#acorn").click(function(){
+
+  function gameLoop() {
+    arr = iterate(arr);
+    print_to_canvas(arr);
+    requestId = window.requestAnimationFrame(gameLoop);
+  }
+
+  document.querySelector("#acorn").addEventListener('click', () => {
     arr = create2Darray(arr.length,arr[0].length, DEAD);
     initialise_acorn(arr);
     print_to_canvas(arr);
   });
-  $("#cross").click(function(){
+
+  document.querySelector("#cross").addEventListener('click', () => {
     arr = create2Darray(arr.length,arr[0].length, DEAD);
     initialise_cross(arr);
     print_to_canvas(arr);
   });
   
-  function gameLoop() {
-    arr = iterate();
-    print_to_canvas(arr);
-    requestId = window.requestAnimationFrame(gameLoop);
-  }
-
-  //Initialise acorn on arr:
   function initialise_acorn(arr){
-    var acorn = [[-3, -1],[-2, -1],[-2, 1],[1, -1],[2, -1],[3, -1]];
-    var x = arr.length/2; //x_pos of acorn.
-    var y = arr[0].length/2; //y_pos of acorn.
-    arr[x][y]=ALIVE;
-    for (var nn = 0; nn < 6; nn++){
-      var dx = acorn[nn][0];
-      var dy = acorn[nn][1];
-      arr[x + dx][y + dy] = ALIVE;
+    var Lx = arr.length;
+    var Ly = arr[0].length;
+    var acorn = [[0, 0],[-3, -1],[-2, -1],[-2, 1],[1, -1],[2, -1],[3, -1]];
+    for (var i = 0; i < acorn.length; i++){
+      var dx = acorn[i][0];
+      var dy = acorn[i][1];
+      arr[Lx/2 + dx][Ly/2 + dy] = ALIVE;
     }
   }
 
-  //Initialise cross on arr:
   function initialise_cross(arr){
     var Lx = arr.length;
     var Ly = arr[0].length;
-    // Top left to bottom right
     for (var x = 0; x < Lx; x++) {
       var y = x;
       if (y >= 0 && y < Ly) {
         arr[x][y] = ALIVE;
       }
     }
-    // Top right to botom left
     for (var x = Lx - 1; x >= 0; x--) {
       var y = Ly - x;
       if (y >= 0 && y < Ly) {
@@ -69,40 +68,36 @@ $(document).ready(function(){
     }
 }
 
-  //Iterate next state of arr:
-  function iterate(){
-    var myNewArray = create2Darray(arr.length,arr[0].length, DEAD);
-    for(var x=0; x < arr.length; x++){
-      for(var y=0; y < arr[0].length; y++){
-        if(arr[x][y] === ALIVE){
-          if (getneighcount(arr, x, y) < 2){
-            myNewArray[x][y] = DEAD; //underpopulation kills cell.
+  // Iterate next state of arr
+  function iterate(oldArray){
+    var Lx = oldArray.length;
+    var Ly = oldArray[0].length;
+    var newArray = create2Darray(Lx, Ly, DEAD);
+    for(var x = 0; x < oldArray.length; x++){
+      for(var y = 0; y < oldArray[0].length; y++){
+        if(oldArray[x][y] === ALIVE){
+          if (getneighcount(oldArray, x, y) < 2){
+            newArray[x][y] = DEAD; // Underpopulation kills cell.
           }
-          else if (getneighcount(arr, x, y) === 2 || getneighcount(arr, x, y) === 3){
-            myNewArray[x][y] = ALIVE; // surivies!
+          else if (getneighcount(oldArray, x, y) === 2 || getneighcount(oldArray, x, y) === 3){
+            newArray[x][y] = ALIVE; // Surivies!
           }
-          else if (getneighcount(arr, x, y)>3){
-            myNewArray[x][y] = DEAD; //overpopulation kills cell.
+          else if (getneighcount(oldArray, x, y) > 3){
+            newArray[x][y] = DEAD; // Overpopulation kills cell.
           }
         }
         else if(arr[x][y] === DEAD){
-          if(getneighcount(arr, x, y) == 3)
+          if(getneighcount(oldArray, x, y) === 3)
           {
-            myNewArray[x][y] = ALIVE; //reproduction.
+            newArray[x][y] = ALIVE; // Reproduction.
           }
         }
       }
     }
-    return myNewArray;
+    return newArray;
   }
 
-  //Print array to screen:
   function print_to_canvas(arr){
-    var canvas = document.querySelector("#myCanvas");
-    canvas.width = arr.length;
-    canvas.height = arr[0].length;
-    var ctx = canvas.getContext("2d");
-
     var myImageData = ctx.getImageData(0,0,arr.length, arr[0].length);
     for(var x=0; x < arr.length; x++){
       for(var y=0; y < arr[0].length; y++){
@@ -124,8 +119,8 @@ $(document).ready(function(){
     ctx.putImageData(myImageData, 0, 0);
   }
 
-  //Get number of ALIVE neighbours of point (x,y): 
-  function getneighcount(arr,x,y){
+  // Get number of ALIVE neighbours for point (x,y)
+  function getneighcount(arr, x, y){
     var neighbourhood = [[1, 0],[1, 1],[0, 1],[-1, 1],[-1, 0],[-1, -1],[0, -1],[1, -1]];
     var nc = 0;
     for (var nn = 0; nn < 8; nn++){
@@ -138,7 +133,7 @@ $(document).ready(function(){
     return nc;
   }
 
-  //Periodic boundary conditions in z-direction:
+  // Periodic boundary conditions in z-direction:
   function pbcz(iz, Lz){
     if (iz >= Lz){
       iz = iz - Lz;
@@ -149,8 +144,8 @@ $(document).ready(function(){
     return iz;
   }
 
-  //Creates a 2D array, initialised to value:
-  function create2Darray(w,h, value){
+  // Creates a 2D array, initialised to value
+  function create2Darray(w, h, value){
     var arr = []
     for(var x=0; x < w; x++){
       arr[x] = [];
