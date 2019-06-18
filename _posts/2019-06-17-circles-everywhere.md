e7945b04b886937ea5604ccecadd051ce3fa52d0
George Pearson <george.pearson@iglu.com>
2019-06-18 21:03:19 +0100

    Add const and let to code example

--- _posts/2019-06-17-circles-everywhere.md
+++ _posts/2019-06-17-circles-everywhere.md
@@ -1957,44 +1957,44 @@ In this post I show off some code to generate an SVG image of packing a set amou
 Here's the code:
 
 ```javascript
-var svgWidth = 600, svgHeight = 600;
-var svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+const svgWidth = 600, svgHeight = 600;
+const svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
 svg.setAttribute("width", svgWidth);
 svg.setAttribute("height", svgHeight);
-var R = 250; // R is the radius of the large circle within which the small circles are to fit.
-var CX = svgWidth / 2; // The x-coordinate of centre of the large circle.
-var CY = svgHeight / 2; // The y-coordinate of centre of the large circle.
-var n = 2000; // n is the maximum number of circles to pack inside the large circle.
-var rho_min = 0.005; // rho_min is rmin/R, where rmin is the minimum packing circle radius.
-var rho_max = 0.05; // rho_max is rmax/R, where rmax is the maximum packing circle radius.
-var rmin = R * rho_min; // The minimum packing circle radius.
-var rmax = R * rho_max; // The maximum packing circle radius.
-var circleColours = ['#993300', '#a5c916', '#00AA66', '#FF9900'];
-var circles = makeCircles(); // Gets a list of small circle coordinates, radii and colours.
+const R = 250; // R is the radius of the large circle within which the small circles are to fit.
+const CX = svgWidth / 2; // The x-coordinate of centre of the large circle.
+const CY = svgHeight / 2; // The y-coordinate of centre of the large circle.
+const n = 2000; // n is the maximum number of circles to pack inside the large circle.
+const rho_min = 0.005; // rho_min is rmin/R, where rmin is the minimum packing circle radius.
+const rho_max = 0.05; // rho_max is rmax/R, where rmax is the maximum packing circle radius.
+const rmin = R * rho_min; // The minimum packing circle radius.
+const rmax = R * rho_max; // The maximum packing circle radius.
+const circleColours = ['#993300', '#a5c916', '#00AA66', '#FF9900'];
+const circles = makeCircles(); // Gets a list of small circle coordinates, radii and colours.
 
-for(var i = 0; i < circles.length; i++){
-    var circle = document.createElementNS(svg.namespaceURI,'circle');
+for(let i = 0; i < circles.length; i++){
+    const circle = document.createElementNS(svg.namespaceURI, 'circle');
     circle.setAttribute('cx', circles[i].cx);
     circle.setAttribute('cy', circles[i].cy);
     circle.setAttribute('r', circles[i].r);
     circle.setAttribute('fill', circles[i].colour);
     svg.appendChild(circle); // Add each circle in the list to the SVG.
 }
-var svgDiv = document.querySelector('#myDiv');
+const svgDiv = document.querySelector('#myDiv');
 svgDiv.appendChild(svg); // Add SVG to page.
 
 function makeCircles(){
-    var circles = [];
-    var radii = [];
+    const circles = [];
+    const radii = [];
     // First choose a set of n random radii and sort them. We use
     // Math.random()*Math.random() to favour small circles.
-    for(var i = 0; i < n; i++){
-        var r = rmin + (rmax - rmin)*(Math.random()*Math.random())
+    for(let i = 0; i < n; i++){
+        const r = rmin + (rmax - rmin)*(Math.random()*Math.random())
         radii.push(r);
     }
     radii.sort().reverse();
     // Do our best to place the circles, larger ones first.
-    for(var i = 0; i < n; i++){
+    for(let i = 0; i < n; i++){
         placeCircle(circles, radii[i]);
     }
     return circles;
@@ -2002,13 +2002,13 @@ function makeCircles(){
 
 function placeCircle(circles, r){
     //The guard number: if we don't place a circle within this number of trials, we give up.
-    var guard = 500;
+    let guard = 500;
     while(guard > 0){
         // Pick a random position, uniformly on the larger circle's interior.
-        var cr = R * Math.sqrt(Math.random());
-        var cphi = 2 * Math.PI * Math.random();
-        var cx = cr * Math.cos(cphi);
-        var cy = cr * Math.sin(cphi);
+        const cr = R * Math.sqrt(Math.random());
+        const cphi = 2 * Math.PI * Math.random();
+        const cx = cr * Math.cos(cphi);
+        const cy = cr * Math.sin(cphi);
         
         if(cr + r < R){
             // The circle fits inside the larger circle.
@@ -2016,7 +2016,7 @@ function placeCircle(circles, r){
                 (existingCircle) => overlapWith(CX + cx, CY + cy, r, existingCircle.cx, existingCircle.cy, existingCircle.r))
             ){
                 // The circle doesn't overlap with any other small circles.
-                var circle = {'cx':CX + cx,'cy':CY + cy, 'r':r, 'colour':circleColours[randomIntFromInterval(0,3)]};
+                const circle = {'cx':CX + cx,'cy':CY + cy, 'r':r, 'colour':circleColours[randomIntFromInterval(0,3)]};
                 circles.push(circle); // Add the circle.
                 return;
             }
@@ -2032,7 +2032,7 @@ function randomIntFromInterval(min, max){
 
 function overlapWith(cx1, cy1, r1, cx2, cy2, r2){
     // Do two circles overlap?
-    var d = Math.hypot(cx1-cx2, cy1-cy2);
+    const d = Math.hypot(cx1-cx2, cy1-cy2);
     return d < r1 + r2;
 }
 ```
--- _scripts/circles-in-a-circle.js
+++ _scripts/circles-in-a-circle.js
@@ -1,45 +1,45 @@
 "use strict";
 (function(){
-    var btnReload = document.querySelector('#reload');
-    var svgWidth = 300, svgHeight = 300;
-    var R = 125; // R is the radius of the large circle within which the small circles are to fit.
-    var CX = svgWidth / 2; // The x-coordinate of centre of the large circle.
-    var CY = svgHeight / 2; // The y-coordinate of centre of the large circle.
-    var n = 1000; // n is the maximum number of circles to pack inside the large circle.
-    var rho_min = 0.01; // rho_min is rmin/R, where rmin is the minimum packing circle radius.
-    var rho_max = 0.05; // rho_max is rmax/R, where rmax is the maximum packing circle radius.
-    var rmin = R * rho_min; // The minimum packing circle radius.
-    var rmax = R * rho_max; // The maximum packing circle radius.
-    var staticCircles = document.querySelector('.staticCircles');
-    var colour1 = document.querySelector('#colour1');
-    var colour2 = document.querySelector('#colour2');
-    var colour3 = document.querySelector('#colour3');
-    var colour4 = document.querySelector('#colour4');
+    const btnReload = document.querySelector('#reload');
+    const svgWidth = 300, svgHeight = 300;
+    const R = 125; // R is the radius of the large circle within which the small circles are to fit.
+    const CX = svgWidth / 2; // The x-coordinate of centre of the large circle.
+    const CY = svgHeight / 2; // The y-coordinate of centre of the large circle.
+    const n = 1000; // n is the maximum number of circles to pack inside the large circle.
+    const rho_min = 0.01; // rho_min is rmin/R, where rmin is the minimum packing circle radius.
+    const rho_max = 0.05; // rho_max is rmax/R, where rmax is the maximum packing circle radius.
+    const rmin = R * rho_min; // The minimum packing circle radius.
+    const rmax = R * rho_max; // The maximum packing circle radius.
+    const staticCircles = document.querySelector('.staticCircles');
+    const colour1 = document.querySelector('#colour1');
+    const colour2 = document.querySelector('#colour2');
+    const colour3 = document.querySelector('#colour3');
+    const colour4 = document.querySelector('#colour4');
 
     btnReload.addEventListener('click', ()=> {
         btnReload.disabled = true;
-        var circleColours = [colour1.value, colour2.value, colour3.value, colour4.value];
-        var svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+        const circleColours = [colour1.value, colour2.value, colour3.value, colour4.value];
+        const svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
         svg.setAttribute("width", svgWidth);
         svg.setAttribute("height", svgHeight);
-        var circles = makeCircles();
-        for(var i = 0; i < circles.length; i++){
-            var circle = document.createElementNS(svg.namespaceURI,'circle');
+        const circles = makeCircles();
+        for(let i = 0; i < circles.length; i++){
+            const circle = document.createElementNS(svg.namespaceURI, 'circle');
             circle.setAttribute('cx', circles[i].cx);
             circle.setAttribute('cy', circles[i].cy);
             circle.setAttribute('r', circles[i].r);
             circle.setAttribute('fill', circles[i].colour);
             svg.appendChild(circle);
         }
-        var canvas = document.querySelector('#myCanvas');
+        const canvas = document.querySelector('#myCanvas');
         canvas.width = svgWidth;
         canvas.height = svgHeight;
-        var ctx = canvas.getContext("2d");
+        const ctx = canvas.getContext("2d");
         ctx.clearRect(0, 0, canvas.width, canvas.height);
-        var svgString = new XMLSerializer().serializeToString(svg);
-        var blob = new Blob([svgString], {type:"image/svg+xml;charset=utf-8"});
-        var url = URL.createObjectURL(blob);
-        var img = new Image();
+        const svgString = new XMLSerializer().serializeToString(svg);
+        const blob = new Blob([svgString], {type:"image/svg+xml;charset=utf-8"});
+        const url = URL.createObjectURL(blob);
+        const img = new Image();
         img.onload = function() {
             ctx.drawImage(img, 0, 0);
             URL.revokeObjectURL(url);
@@ -50,17 +50,17 @@
         canvas.style.display = 'block';
     
         function makeCircles(){
-            var circles = [];
-            var radii = [];
+            const circles = [];
+            const radii = [];
             // First choose a set of n random radii and sort them. We use
             // Math.random()*Math.random() to favour small circles.
-            for(var i = 0; i < n; i++){
-                var r = rmin + (rmax - rmin)*(Math.random()*Math.random())
+            for(let i = 0; i < n; i++){
+                const r = rmin + (rmax - rmin)*(Math.random()*Math.random())
                 radii.push(r);
             }
             radii.sort().reverse();
             // Do our best to place the circles, larger ones first.
-            for(var i=0; i<n; i++){
+            for(let i = 0; i < n; i++){
                 placeCircle(circles, radii[i]);
             }
             return circles;
@@ -68,18 +68,18 @@
         
         function placeCircle(circles, r){
             //The guard number: if we don't place a circle within this number of trials, we give up.
-            var guard = 500;
+            let guard = 500;
             while(guard > 0){
                 // Pick a random position, uniformly on the larger circle's interior.
-                var cr = R * Math.sqrt(Math.random());
-                var cphi = 2 * Math.PI * Math.random();
-                var cx = cr * Math.cos(cphi);
-                var cy = cr * Math.sin(cphi);
+                const cr = R * Math.sqrt(Math.random());
+                const cphi = 2 * Math.PI * Math.random();
+                const cx = cr * Math.cos(cphi);
+                const cy = cr * Math.sin(cphi);
                 
                 if(cr + r < R){
                     // The circle fits inside the larger circle.
                     if(!circles.some((existingCircle) => overlapWith(CX + cx, CY + cy, r, existingCircle.cx, existingCircle.cy, existingCircle.r))){
-                        var circle = {'cx':CX + cx,'cy':CY + cy, 'r':r, 'colour':circleColours[randomIntFromInterval(0,3)]};
+                        const circle = {'cx':CX + cx,'cy':CY + cy, 'r':r, 'colour':circleColours[randomIntFromInterval(0,3)]};
                         circles.push(circle);
                         return;
                     }
@@ -93,7 +93,7 @@
         }
         
         function overlapWith(cx1, cy1, r1, cx2, cy2, r2){
-            var d = Math.hypot(cx1-cx2, cy1-cy2);
+            const d = Math.hypot(cx1-cx2, cy1-cy2);
             return d < r1 + r2;
         }
     });
